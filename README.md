942a391657a9f5d783c2b41a06888bcfe97cdb8a
craigmindset <irediacraig@gmail.com>
2023-03-08 10:59:02 +0100

    my_python_code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python_birth_year_check
+this is a python simple date of birth python program
---
+++ ne.py
@@ -0,0 +1,6 @@
+#this program print a clinic data
+name = input("please insert your name here: ")
+age = input("what is your age? ")
+birth_year = int(age)
+current_year = int('2023')
+print(str("you were born on the") , int(2023) - birth_year )
